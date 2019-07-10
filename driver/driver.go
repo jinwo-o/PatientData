a167b474ec28dbedf6a01ba1e124b5ad7b927f6e
Richard de Borja <richard.deborja@uhnresearch.ca>
2019-07-10 14:40:02 -0400

    uncommented db connection

--- driver/driver.go
+++ driver/driver.go
@@ -17,9 +17,9 @@ var dbConn = &DB{}
 // ConnectSQL ...
 func ConnectSQL(host, port, uname, pass, dbname string) (*DB, error) {
 	// For local use, without docker database
-	d, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1)/patients")
+	//d, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1)/patients")
 	// With docker database
-	// d, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/patients")
+	d, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/patients")
 
 	if err != nil {
 		panic(err)
