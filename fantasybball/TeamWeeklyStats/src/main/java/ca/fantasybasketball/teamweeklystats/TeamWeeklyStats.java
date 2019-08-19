5c7c182e1da6358fa364a7f820ddc14065473ac1
jinwo-o <jinwoo.hwang99@gmail.com>
2019-08-19 11:18:18 -0400

    Added Comments

---
+++ fantasybball/TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
@@ -0,0 +1,35 @@
+package ca.fantasybasketball.teamweeklystats;
+//
+import ca.fantasybasketball.teamweeklystats.model.Player;
+import ca.fantasybasketball.teamweeklystats.stats.PlayerWeeklyTotal;
+import ca.fantasybasketball.teamweeklystats.readers.ReadCSV;
+import ca.fantasybasketball.teamweeklystats.model.Roster;
+import ca.fantasybasketball.teamweeklystats.stats.AddStats;
+import ca.fantasybasketball.teamweeklystats.model.Stats;
+import java.io.IOException;
+
+
+//
+public class TeamWeeklyStats {
+
+    public static void main(String[] args) throws IOException {
+        Roster roster = ReadCSV.CSV_Reader("/users/jin/work/fantasybball/FantasyCSV.csv");
+        System.out.println("Yessir");
+        Stats totalStats = new Stats();
+        for(Player player : roster.getPlayers()) {
+            Stats stats = PlayerWeeklyTotal.calculateWeeklyTotal(player);
+            totalStats = AddStats.total(totalStats, stats);
+        }
+
+        System.out.println("Minutes: "+ totalStats.getMinutes());
+        System.out.println("Threes: " + totalStats.getThrees());
+        System.out.println("Rebounds: " + totalStats.getRebounds());
+        System.out.println("Assists: " + totalStats.getAssists());
+        System.out.println("Steals: " + totalStats.getSteals());
+        System.out.println("Blocks: " + totalStats.getBlocks());
+        System.out.println("Turnovers: " + totalStats.getTurnovers());
+        System.out.println("DD: " + totalStats.getDD());
+        System.out.println("Points: " + totalStats.getPoints());
+    }
+
+}
